87c3f4dc2a40ec6c1ed85ffc36ff3979b6a0d7a8
Elias Garcia <eliasgarcia971@gmail.com>
2026-02-17 18:12:10 -0600

    feat(Post MAVE): Add new post and refactor image on MarkdownPostLayout

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,18 +3,19 @@ import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: './src/blog' }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string()
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: z.object({
+        url: image(),
+        alt: z.string(),
+      }),
+      tags: z.array(z.string()),
     }),
-    tags: z.array(z.string())
-  })
 });
 
 export const collections = { blog };
